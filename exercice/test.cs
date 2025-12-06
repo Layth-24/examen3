b0dd1eb8c8e81b3c7aad1b40fd47ace3ab53c4c8
Layth-24 <2746596@collegelacite.ca>
2025-12-06 00:06:10 -0500

    commit pou modifier le fichier test
    ce commit permet de modifier le fichier test de la branche1

--- exercice/test.cs
+++ exercice/test.cs
@@ -7,6 +7,12 @@ namespace ExerciceGit
         static void Main(string[] args)
         {
             Console.WriteLine("Bonjour, ceci est le fichier test en C#.");
+            AfficherMessageBranche1();
+        }
+
+        static void AfficherMessageBranche1()
+        {
+            Console.WriteLine("Ceci est la modification du message.");
         }
     }
 }
