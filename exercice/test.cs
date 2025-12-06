802a237f142e643edc489069a7bf145c59c39b2e
Layth-24 <2746596@collegelacite.ca>
2025-12-05 23:29:03 -0500

    commit pour ajout du dossier exercice et du fichier test dans git online
    ceci est un commit d'jout du dossier exercice et du fichier test

---
+++ exercice/test.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ExerciceGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bonjour, ceci est le fichier test en C#.");
+        }
+    }
+}
